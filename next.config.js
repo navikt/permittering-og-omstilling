a5c9fe4420e72404cc85f4fbc3daa14a07388880
Mattias Lundmark <lundmark.mattias@gmail.com>
2021-09-09 14:35:59 +0200

    Mer basePath

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
-  // basePath: '/permittering-og-omstilling',
+  basePath: '/permittering-og-omstilling',
   // assetPrefix: '/permittering-og-omstilling',
   // publicRuntimeConfig: {
   //   basePath: '/permittering-og-omstilling',
