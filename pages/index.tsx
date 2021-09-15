b21d6c5bde8aa26fbd39134a2bbcded891cd87f5
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-09-15 11:05:07 +0200

    stylet forsiden minimalt

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,20 @@
 import type { NextPage } from "next";
 import Link from "next/link";
 import "@navikt/ds-css";
+import {LinkPanel} from "@navikt/ds-react";
+import Container from "../components/container/Container";
+import styles from './index.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div className="hovedside">
-      <Link href="/permittering">Veiviser for permittering</Link>
-      <Link href="/omstilling">Omstilling og nedbemanning</Link>
-      <Link href="/oppsigelse">Oppsigelse</Link>
-    </div>
+      <div className={styles.container}>
+    <Container>
+
+      <LinkPanel href="/permittering">Veiviser for permittering</LinkPanel>
+      <LinkPanel href="/omstilling">Omstilling og nedbemanning</LinkPanel>
+      <LinkPanel href="/oppsigelse">Oppsigelse</LinkPanel>
+    </Container>
+          </div>
   );
 };
 
