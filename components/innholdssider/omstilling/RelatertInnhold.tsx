8ba2bd12ff716f3f07d2793dee18cd65af1564c9
André Mossige <andre.mossige@nav.no>
2021-09-20 12:33:46 +0200

    Fiks kompileringsfeil

--- components/innholdssider/omstilling/RelatertInnhold.tsx
+++ components/innholdssider/omstilling/RelatertInnhold.tsx
@@ -1,16 +1,23 @@
 import { Accordion, BodyShort, Link } from "@navikt/ds-react";
 
 const RelatertInnhold = () => {
-    return (
-        <Accordion heading="Relatert innhold">
-            <BodyShort>
-              <Link href="/oppsigelse">Oppsigelse</Link>
-            </BodyShort>
-            <BodyShort>
-              <Link href="https://www.nav.no/no/person/arbeid/dagpenger-ved-arbeidsloshet-og-permittering/ventelonn">Ventelønn</Link>
-            </BodyShort>
-          </Accordion>
-    );
-}
+  return (
+    <Accordion>
+      <Accordion.Item>
+        <Accordion.Header>Relatert innhold</Accordion.Header>
+        <Accordion.Content>
+          <BodyShort>
+            <Link href="/oppsigelse">Oppsigelse</Link>
+          </BodyShort>
+          <BodyShort>
+            <Link href="https://www.nav.no/no/person/arbeid/dagpenger-ved-arbeidsloshet-og-permittering/ventelonn">
+              Ventelønn
+            </Link>
+          </BodyShort>
+        </Accordion.Content>
+      </Accordion.Item>
+    </Accordion>
+  );
+};
 
 export default RelatertInnhold;
