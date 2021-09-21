d051da83e701cd07006ffee3fed1fe587c276173
André Mossige <andre.mossige@nav.no>
2021-09-21 11:58:44 +0200

    Start på implementering av henting av data fra Sanity

--- pages/permittering.tsx
+++ pages/permittering.tsx
@@ -1,8 +1,17 @@
-import type { NextPage } from "next";
+import type { GetServerSideProps, NextPage } from "next";
 import Permitteringsside from "../components/innholdssider/permittering/Permitteringsside";
+import { sanityClient } from "../poagpermitteringogomstilling/sanity";
 
 const Permittering: NextPage = () => {
   return <Permitteringsside />;
+};
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const query = "*";
+  const vanligeSpørsmål = await sanityClient.fetch(query);
+
+  console.log("Fik respons fra sanity: ", vanligeSpørsmål);
+  return { props: { tekst: "hei" } };
 };
 
 export default Permittering;
