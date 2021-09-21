6611c061f2b8678927508ef79bc0eee47ad70ba2
Hilde Steinbru Heggstad <hilde.steinbru.heggstad@nav.no>
2021-09-21 12:50:17 +0200

    henter data fra sanity lokalt

--- pages/permittering.tsx
+++ pages/permittering.tsx
@@ -1,13 +1,13 @@
 import type { GetServerSideProps, NextPage } from "next";
 import Permitteringsside from "../components/innholdssider/permittering/Permitteringsside";
-import { sanityClient } from "../poagpermitteringogomstilling/sanity";
+import { sanityClient } from "../sanity/sanity";
 
 const Permittering: NextPage = () => {
   return <Permitteringsside />;
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-  const query = "*";
+  const query = "*[_type == \"vanligsporsmal\"]";
   const vanligeSpørsmål = await sanityClient.fetch(query);
 
   console.log("Fik respons fra sanity: ", vanligeSpørsmål);
