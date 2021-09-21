1532123bbc334ce97bde1daadf1adf99e02107b8
Mattias Lundmark <lundmark.mattias@gmail.com>
2021-09-21 10:00:17 +0200

    Testet legg til schema

---
+++ poagpermitteringogomstilling/schemas/vanligSporsmal.js
@@ -0,0 +1,17 @@
+export default {
+    title: "Vanlig spørsmål",
+    name: "vanligsporsmal",
+    type: "document",
+    fields: [
+        {
+            title: "Spørsmål",
+            name: "sporsmal",
+            type: "string"
+        },
+        {
+            title: "Svar",
+            name: "svar",
+            type: "string"
+        }
+    ]
+}
