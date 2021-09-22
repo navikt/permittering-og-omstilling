8bd118005cf898ad39821c05c716bf177110710c
André Mossige <andre.mossige@nav.no>
2021-09-22 13:31:29 +0200

    Legg til tema i stegForSteg

--- sanity/schemas/stegForSteg.js
+++ sanity/schemas/stegForSteg.js
@@ -1,24 +1,28 @@
 export default {
-    title: "Steg for steg",
-    name: "stegforsteg",
-    type: "document",
-    fields: [
+  title: "Steg for steg",
+  name: "stegforsteg",
+  type: "document",
+  fields: [
+    {
+      title: "Tittel",
+      name: "tittel",
+      type: "string",
+    },
+    {
+      type: "array",
+      name: "steg",
+      of: [
         {
-            title: "Tittel",
-            name: "tittel",
-            type: "string"
+          title: "Steg",
+          type: "steg",
         },
-        {
-            type: 'array',
-            name: 'steg',
-            of: [
-                {
-                    title: 'Steg',
-                    type: 'steg'
-                },
-            ]
-        }
-
-    ]
-}
-
+      ],
+    },
+    {
+      title: "Tema",
+      name: "tema",
+      type: "reference",
+      to: [{ type: "tema" }],
+    },
+  ],
+};
