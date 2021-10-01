2c8f4944ce386ef4c78e740bc662098e261694ba
André Mossige <andre.mossige@nav.no>
2021-10-01 10:52:15 +0200

    Forenkle innholdsmeny

--- components/innholdssider/permittering/innholdsmeny/Innholdsmeny.tsx
+++ components/innholdssider/permittering/innholdsmeny/Innholdsmeny.tsx
@@ -1,30 +1,33 @@
 import styles from "./innholdsmeny.module.css";
-import {AccordionMenu, Heading} from "@navikt/ds-react";
+import { AccordionMenu, Heading } from "@navikt/ds-react";
 import { permitteringInnhold } from "../Permitteringsside";
-import {FunctionComponent} from "react";
+import { FunctionComponent } from "react";
+
 type InnholdsmenyProps = {
-    nåværendeHash: string|undefined
-    setNåværendeHash: (hash: string) => void;
+  nåværendeHash?: string;
+  setNåværendeHash: (hash: string) => void;
 };
 
-const Innholdsmeny: FunctionComponent<InnholdsmenyProps> = ({nåværendeHash,setNåværendeHash}) => {
-
+const Innholdsmeny: FunctionComponent<InnholdsmenyProps> = ({
+  nåværendeHash,
+  setNåværendeHash,
+}) => {
   return (
-      <AccordionMenu className={styles.innholdsmeny}>
-          <Heading size="small" className={styles.tittel}>
-              Innhold på siden
-          </Heading>
-          {Object.values(permitteringInnhold).map((innhold) => {
-                  return  nåværendeHash?.includes(innhold.anker) ?
-              <AccordionMenu.Item active href={`#${innhold.anker}`} key={innhold.anker}>
-                  {innhold.tittel}
-              </AccordionMenu.Item>
-                      :
-                      <AccordionMenu.Item onClick={() => setNåværendeHash(innhold.anker)} href={`#${innhold.anker}`} key={innhold.anker}>
-                          {innhold.tittel}
-                      </AccordionMenu.Item>
-          })}
-      </AccordionMenu>
+    <AccordionMenu className={styles.innholdsmeny}>
+      <Heading size="small" className={styles.tittel}>
+        Innhold på siden
+      </Heading>
+      {Object.values(permitteringInnhold).map((innhold) => (
+        <AccordionMenu.Item
+          active={nåværendeHash?.includes(innhold.anker)}
+          onClick={() => setNåværendeHash(innhold.anker)}
+          href={`#${innhold.anker}`}
+          key={innhold.anker}
+        >
+          {innhold.tittel}
+        </AccordionMenu.Item>
+      ))}
+    </AccordionMenu>
   );
 };
 
