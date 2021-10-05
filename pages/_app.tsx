3899ce348c63366eb0094f25fea2cfc8672da78e
André Mossige <andre.mossige@nav.no>
2021-10-05 09:27:27 +0200

    Prøv å fjerne HotJar siden vi kanskje får det fra dekoratøren

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,9 @@
 import { AppProps } from "next/app";
 import "../styles/globals.css";
 import "@navikt/ds-css";
-import Script from "next/script";
-import { basePath } from "./index";
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
-  return (
-    <>
-      <Script src={`/${basePath}/hotjar.js`} />
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 };
 
 export default MyApp;
