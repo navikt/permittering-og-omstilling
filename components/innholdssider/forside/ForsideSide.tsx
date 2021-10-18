afd93c8893d215ba4467403fbdd63a22f568a696
André Mossige <andre.mossige@nav.no>
2021-10-18 12:36:46 +0200

    Legg til seksjon for introduksjon

--- components/innholdssider/forside/ForsideSide.tsx
+++ components/innholdssider/forside/ForsideSide.tsx
@@ -1,17 +1,33 @@
 import { NextPage } from "next";
 import React from "react";
-import { LinkPanel, PageHeader } from "@navikt/ds-react";
-import styles from "./forside.module.css";
+import { BodyLong, LinkPanel, PageHeader } from "@navikt/ds-react";
 import HvaKanNavHjelpeDegMed from "./HvaKanNavHjelpeDegMed/HvaKanNavHjelpeDegMed";
+import Container from "../../container/Container";
+
+import styles from "./forside.module.css";
 
 const ForsideSide: NextPage = () => {
   return (
     <div>
-      <PageHeader description="Her er beskrivelsen">Forside</PageHeader>
-      <div>Her er introduksjonen til permittering/omstilling...</div>
-      <LinkPanel href="/permittering-og-omstilling/permittering#hvordan-permittere">
-        <LinkPanel.Title>Hvordan permittere ansatte</LinkPanel.Title>
-      </LinkPanel>
+      <PageHeader description="Her er beskrivelsen">
+        Permittering og omstilling
+      </PageHeader>
+
+      <div className={styles.intro}>
+        <Container>
+          <BodyLong spacing>
+            Litt om omstilling permittering og nedmannning - hva er det AG kan
+            finne ut av her? Hvilken verdi får AG hvis de leser informasjonen på
+            disse sidene?
+          </BodyLong>
+          <LinkPanel
+            href="/permittering-og-omstilling/permittering#hvordan-permittere"
+            className={styles.hvordanPermittere}
+          >
+            <LinkPanel.Title>Hvordan permittere ansatte</LinkPanel.Title>
+          </LinkPanel>
+        </Container>
+      </div>
 
       <HvaKanNavHjelpeDegMed />
     </div>
