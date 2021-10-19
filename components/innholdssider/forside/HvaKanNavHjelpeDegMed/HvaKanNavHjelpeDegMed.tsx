4d060be431b7b4e209aacd1f0185503c04a37739
André Mossige <andre.mossige@nav.no>
2021-10-19 14:24:46 +0200

    Legg til kategori på hva nav kan hjelpe med

--- components/innholdssider/forside/HvaKanNavHjelpeDegMed/HvaKanNavHjelpeDegMed.tsx
+++ components/innholdssider/forside/HvaKanNavHjelpeDegMed/HvaKanNavHjelpeDegMed.tsx
@@ -17,16 +17,19 @@ const HvaKanNavHjelpeDegMed: FunctionComponent<Props> = () => {
           tittel="Hvordan permittere ansatte"
           href="/permittering"
           innhold="Skal du permittering ansatte melder du fra til NAV så tidlig som mulig"
+          kategori="kategori"
         />
         <Lenkekort
           tittel="Skal du omstille?"
           href="/omstilling"
           innhold="Skal du omstille"
+          kategori="kategori"
         />
         <Lenkekort
           tittel="Trenger du hjelp fra NAV eller andre?"
           href="/hjelp"
           innhold="Se på muligheter for å søke om støtte fra NAV"
+          kategori="kategori"
         />
       </div>
     </div>
--- components/lenkekort/Lenkekort.tsx
+++ components/lenkekort/Lenkekort.tsx
@@ -1,32 +1,36 @@
 import React, { FunctionComponent } from "react";
 import styles from "./Lenkekort.module.css";
-import { Heading } from "@navikt/ds-react";
+import { BodyShort, Heading } from "@navikt/ds-react";
 import Link from "next/link";
 
 type Props = {
   tittel: string;
   href: string;
   innhold: string;
-  imgsrc?: string;
+  // imgsrc: string;
+  kategori: string;
 };
 
 const Lenkekort: FunctionComponent<Props> = ({
   tittel,
   href,
   innhold,
-  imgsrc,
+  kategori,
 }) => {
   return (
     <Link href={href}>
-      <a className={styles.lenkekort} role={"link"}>
+      <a className={styles.lenkekort} role="link">
         <img
           className={styles.ikon}
-          src={"/permittering-og-omstilling/eksempel.svg"}
+          src="/permittering-og-omstilling/eksempel.svg"
         />
-        <Heading spacing className={styles.heading} size={"medium"}>
+        <Heading spacing className={styles.heading} size="medium">
           {tittel}
         </Heading>
-        <div className={styles.innholdstekst}>{innhold}</div>
+        <BodyShort className={styles.innholdstekst} spacing>
+          {innhold}
+        </BodyShort>
+        <BodyShort className={styles.kategori}>{kategori}</BodyShort>
       </a>
     </Link>
   );
